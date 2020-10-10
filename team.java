5489b02fef86abe4f5d100678e136b73718889e5
Azam <70405426+azamahmad@users.noreply.github.com>
2020-10-09 18:55:22 -0600

    Added do while for input validation

--- team.java
+++ team.java
@@ -4,22 +4,39 @@ public class team {
     String teamName;
     String color;
     int id;
+    boolean validated;
 
     team(){
         Scanner input = new Scanner(System.in);
-        System.out.print("Input team name:");
-        teamName = input.nextLine();
+
+        do{
+            System.out.print("Input team name:");
+            teamName = input.nextLine();
+        } while(teamName.length() == 0);
         System.out.println("Team name: " + teamName);
-        System.out.println("Input team ID:");
-        id = Integer.parseInt(input.nextLine());
+
+        do {
+            System.out.println("Create a team ID:");
+            try {
+                id = Integer.parseInt(input.nextLine());
+                validated = true;
+            } catch (java.lang.NumberFormatException e) {
+                System.out.println("ID must be an integer");
+                validated = false;
+            }
+        } while (id == 0);
         System.out.println("ID is " + id);
-        System.out.println("Input team color:");
-        color = input.nextLine();
+
+        do{
+            System.out.println("Input team color:");
+            color = input.nextLine();
+        }while(color.length() == 0);
+
         System.out.println("Color is " + color);
     }
 
     void print(){
-        System.out.println("Username is " + teamName);
+        System.out.println("Team name is " + teamName);
         System.out.println("Color is " + color);
     }
 
