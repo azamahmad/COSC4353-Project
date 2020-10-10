db194fec9b0fcb15505de94b92332ad169726f49
GelatoBAE <jxhernandez@uh.edu>
2020-10-09 19:47:38 -0500

    Added input verification & Add. Info. field

--- member.java
+++ member.java
@@ -4,23 +4,41 @@ public class member {
     String name;
     int id;
     String color;
+    String additional;
+    boolean validated;
 
     member(){
         Scanner input = new Scanner(System.in);
-        System.out.print("Input member name:");
-        name = input.nextLine();
+        do {
+            System.out.println("Input member name:");
+            name = input.nextLine();
+        } while(name.length() == 0);
+
         System.out.println("Username is " + name);
-        System.out.println("Input member ID:");
-        id = Integer.parseInt(input.nextLine());
+        do {
+            System.out.println("Input member ID:");
+            try {
+                id = Integer.parseInt(input.nextLine());
+                validated = true;
+            } catch (java.lang.NumberFormatException e) {
+                System.out.println("ID must be an integer");
+                validated = false;
+            }
+        } while (id == 0);
         System.out.println("ID is " + id);
-        System.out.println("Input member color:");
-        color = input.nextLine();
+        do {
+            System.out.println("Input member color:");
+            color = input.nextLine();
+        } while (color.length() == 0);
         System.out.println("Color is " + color);
+        System.out.println("Additional member information (can leave empty):");
+        additional = input.nextLine();
     };
 
     void print(){
         System.out.println("Username is " + name);
         System.out.println("ID is " + id);
         System.out.println("Color is " + color);
+        System.out.println("Additional member information: " + additional);
     }
 }
